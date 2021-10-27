97f9d156d8cc098d8384c2ca33c2f71287997359
UmaKamsala <kamsalauma@gmail.com>
2021-10-27 15:36:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_Wage_Comp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Computation Program");
+        }
+    }
+}
